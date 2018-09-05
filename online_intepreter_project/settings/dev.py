808eb817879213256c204cc769acfca45bd7266e
unknown <Kevin Ou>
2018-09-04 23:22:42 -0700

    change dev debug to false

--- online_intepreter_project/settings/dev.py
+++ online_intepreter_project/settings/dev.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY =  os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.py-online.ca-central-1.elasticbeanstalk.com','127.0.0.1', 'localhost']
 
